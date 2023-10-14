9e09736a09d0285d6d4e8ca54eaab083567e1bce
darknessInMe <vladyslav.didukh.22@gmail.com>
2023-10-15 02:12:21 +0300

    get rid of extra app component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { render } from 'react-dom';
-import App from 'App';
 import 'index.scss';
+import { render } from 'react-dom';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from 'pages/Home';
+import { Game } from 'pages/Game';
+import { NotFound } from 'pages/NotFound';
 
 const Root: React.FC = () => {
     return (
         <DndProvider backend={HTML5Backend}>
-            <App />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/game' element={<Game />}/>
+                    <Route path='*' element={<NotFound />}/>
+                </Routes>
+            </BrowserRouter>
         </DndProvider>
     );
 };
