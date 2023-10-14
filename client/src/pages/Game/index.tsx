471d79d960f4f886b42e93227717dbe4215dae25
darknessInMe <vladyslav.didukh.22@gmail.com>
2023-10-15 02:32:04 +0300

    base home layout

--- client/src/pages/Game/index.tsx
+++ client/src/pages/Game/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { BoardSection } from 'components/BoardSection';
 import { PieceBankSection } from 'components/PieceBankSection';
 import { RootProvider } from 'context/RootContext';
+import clsx from 'clsx';
 
 export const Game = () => {
     return (
         <RootProvider>
-            <div className='screen'>
+            <div className={clsx('screen', 'page')}>
                 <BoardSection />
                 <PieceBankSection />
             </div>
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import clsx from 'clsx';
 
 export const Home = () => {
     return (
-        <div>Hello world</div>
+        <div className={clsx('home', 'page')}>
+            <div className='home__content'>
+                <h1>Stratego</h1>
+                <section>
+                    <h2>Create an own game room</h2>
+                    <button>Create</button>
+                </section>
+                <hr />
+                <section>
+                    <h2>Join to existing game room</h2>
+                    <div>
+                        <input />
+                        <button>Join</button>
+                    </div>
+                </section>
+            </div>
+        </div>
     );
 };
