be265c3a395f1c6d3135f3d056885449d7e6e452
darknessInMe <vladyslav.didukh.22@gmail.com>
2023-10-14 17:23:43 +0300

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { room } from './routes/room';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/room', room);
 
