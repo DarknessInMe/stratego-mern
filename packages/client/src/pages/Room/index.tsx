55f9bbbf9eadaed3cab9b378ee4d9dec2d96b9d5
darknessInMe <vladyslav.didukh.22@gmail.com>
2023-10-22 02:25:56 +0300

    frontend: basic styling of creating room

--- packages/client/src/pages/Room/index.tsx
+++ packages/client/src/pages/Room/index.tsx
@@ -1,7 +1,51 @@
+import clsx from 'clsx';
+import { useSessionContext } from 'context/SessionContext';
 import React, { memo } from 'react';
 
 export const Room = memo(() => {
+    const { session } = useSessionContext();
+    const link = `${window.origin}/join/${session.id}`;
+
+    const onCopy = () => {
+        navigator.clipboard.writeText(link);
+    };
+
     return (
-        <div>index</div>
+        <div className='page'>
+            <div className='room-page'>
+                <section className='room-page__link-section'>
+                    <p>Invite link</p>
+                    <input 
+                        disabled
+                        value={link}
+                        className='room-page__link-input'
+                    />
+                    <button
+                        onClick={onCopy}
+                    >
+                        Copy
+                    </button>
+                </section>
+                <section className='room-page__users-section'>
+                    {session.users.map((user, index) => (
+                        <div
+                            key={user.id}
+                            className='user-item'
+                        >
+                            <span>
+                                <strong>User {index}</strong>
+                            </span>
+                            <span>{user.id === session.ownerId ? 'Owner' : ''}</span>
+                            <span
+                                className={clsx('user-item__status', user.isReady && 'user-item_ready')}
+                            >
+                                {user.isReady ? 'Ready' : 'Not ready'} 
+                            </span>
+                            <button>{user.isReady ? 'Cancel' : 'Get ready'}</button>
+                        </div>
+                    ))}
+                </section>
+            </div>
+        </div>
     );
 });
