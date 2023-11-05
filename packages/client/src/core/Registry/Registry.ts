544782c404649dc7768ee193d9b0f93fd4afb842
darknessInMe <vladyslav.didukh.22@gmail.com>
2023-11-05 16:25:08 +0200

    enhance registry

--- packages/client/src/core/Registry/Registry.ts
+++ packages/client/src/core/Registry/Registry.ts
@@ -1,15 +1,25 @@
 import { TeamsEnum } from '@stratego/common';
 import { BasePiece } from 'core/Pieces';
+import { PieceNameEnum } from 'shared/enums';
 
 interface IPieces {
     [TeamsEnum.BLUE_TEAM]: Map<string, BasePiece>,
     [TeamsEnum.RED_TEAM]: Map<string, BasePiece>,
 }
 
+interface IFlagPieces {
+    [TeamsEnum.BLUE_TEAM]: string,
+    [TeamsEnum.RED_TEAM]: string,
+}
+
 export class Registry {
     pieces: IPieces = {
         [TeamsEnum.BLUE_TEAM]: new Map(),
         [TeamsEnum.RED_TEAM]: new Map(),
+    };
+    flagPieces: IFlagPieces = {
+        [TeamsEnum.BLUE_TEAM]: '',
+        [TeamsEnum.RED_TEAM]: '',
     };
     piecesToTeam = new Map<string, TeamsEnum>();
 
@@ -18,6 +28,10 @@ export class Registry {
     registerPiece(piece: BasePiece) {
         this.piecesToTeam.set(piece.id, piece.team);
         this.pieces[piece.team].set(piece.id, piece);
+
+        if (piece.rankName === PieceNameEnum.FLAG) {
+            this.flagPieces[piece.team] = piece.id;
+        }
     }
 
     deletePiece(piece: BasePiece) {
@@ -25,6 +39,10 @@ export class Registry {
 
         this.piecesToTeam.delete(piece.id);
         this.pieces[team].delete(piece.id);
+
+        if (piece.rankName === PieceNameEnum.FLAG) {
+            this.flagPieces[piece.team] = '';
+        }
     }
 
     getPieceById(id: string) {
